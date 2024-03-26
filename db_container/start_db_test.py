bf7dc7281e778b8843590f61798da8b865a039b1
Kevin Aguero <kevin.nmn.aguero@gmail.com>
2024-03-25 22:37:27 -0600

    working on testing now

--- db_container/start_db_test.py
+++ db_container/start_db_test.py
@@ -6,7 +6,16 @@ import start_db as test_module
 Test creating volume and database
 """
 class TestContainer(unittest.TestCase):
-    pass
+
+    @classmethod
+    def setUp(self):
+        # Create the container object
+        self.container = test_module.ContainerCreate()
+
+    def test_volume_create(self):
+        self.container.create_volume(volume_name='testcladis')
+        # self.assertEqual(self.container.volume_name, 'testcladis')
+
 
 
 """
